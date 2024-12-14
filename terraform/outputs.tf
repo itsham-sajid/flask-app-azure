db4760f61bb7c35bc98ecbdbe23274a38e4a4b1e
Itsham Sajid <99727892+itsham-sajid@users.noreply.github.com>
2024-12-14 20:20:11 +0000

    Delete

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg-flask-app.name
-}
-
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.flask-app-vm.public_ip_address
-}
